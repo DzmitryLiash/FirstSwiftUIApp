a5c96458cecea0e8f31b2da089a969f64067e4c2
Dzmitry Liashchou <liashchou.d@gmail.com>
2021-12-29 19:46:55 +0100

    section 3 combine views using stacks

--- Landmarks/ContentView.swift
+++ Landmarks/ContentView.swift
@@ -8,16 +8,25 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
-        Text("Turtle Rock")
-            .font(.title)
-            
+  var body: some View {
+    VStack(alignment: .leading) {
+      Text("Turtle Rock")
+        .font(.title)
+      HStack {
+        Text("Joshua Tree National Park")
+          .font(.subheadline)
+        Spacer()
+        Text("California")
+          .font(.subheadline)
+      }
     }
+    .padding()
+  }
 }
 
 struct ContentView_Previews: PreviewProvider {
-    static var previews: some View {
-        ContentView()
-.previewInterfaceOrientation(.portrait)
-    }
+  static var previews: some View {
+    ContentView()
+      .previewInterfaceOrientation(.portrait)
+  }
 }
