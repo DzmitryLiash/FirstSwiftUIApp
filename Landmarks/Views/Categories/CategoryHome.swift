c825f3523bc3ca3986dd4365189d1bbaa94ca5a9
Dzmitry Liashchou <liashchou.d@gmail.com>
2022-01-02 21:23:48 +0100

    section 1 display a user profile

--- Landmarks/Views/Categories/CategoryHome.swift
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -9,6 +9,7 @@ import SwiftUI
 
 struct CategoryHome: View {
   @EnvironmentObject var modelData: ModelData
+  @State private var showingProfile = false
   
   var body: some View {
     NavigationView {
@@ -19,15 +20,26 @@ struct CategoryHome: View {
                     .frame(height: 200)
                     .clipped()
                     .listRowInsets(EdgeInsets())
-
+        
         
         ForEach(modelData.categories.keys.sorted(), id: \.self) { key in
           CategoryRow(categoryName: key, items: modelData.categories[key]!)
         }
         .listRowInsets(EdgeInsets())
       }
+      .listStyle(.inset)
       .navigationTitle("Featured")
-  
+      .toolbar {
+        Button {
+          showingProfile.toggle()
+        } label: {
+          Label("User Profile", systemImage: "person.crop.circle")
+        }
+      }
+      .sheet(isPresented: $showingProfile) {
+        ProfileHost()
+          .environmentObject(modelData)
+      }
     }
   }
 }
