aca8ff9f83f65a509fda0e71f8938fa4b0e9d9d5
Dzmitry Liashchou <liashchou.d@gmail.com>
2022-01-02 20:40:38 +0100

    section 1 add a category view

---
+++ Landmarks/Views/Categories/CategoryHome.swift
@@ -0,0 +1,24 @@
+//
+//  CategoryHome.swift
+//  Landmarks
+//
+//  Created by Дмитрий Лещёв on 02/01/2022.
+//
+
+import SwiftUI
+
+struct CategoryHome: View {
+  var body: some View {
+    NavigationView {
+      Text("Hello, World!")
+        .navigationTitle("Featured")
+      
+    }
+  }
+}
+
+struct CategoryHome_Previews: PreviewProvider {
+  static var previews: some View {
+    CategoryHome()
+  }
+}
