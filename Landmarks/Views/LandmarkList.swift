aeec5b74ae8c0667752f54b6fd053a0c7234f281
Dzmitry Liashchou <liashchou.d@gmail.com>
2022-01-02 18:27:36 +0100

    section 2 filter the list view

--- Landmarks/Views/LandmarkList.swift
+++ Landmarks/Views/LandmarkList.swift
@@ -8,13 +8,21 @@
 import SwiftUI
 
 struct LandmarkList: View {
+  @State private var showFavoritesOnly = true
+  
+  var filteredLandmarks: [Landmark] {
+       landmarks.filter { landmark in
+           (!showFavoritesOnly || landmark.isFavorite)
+       }
+   }
+  
   var body: some View {
     NavigationView{
-      List(landmarks) { Landmark in
+      List(filteredLandmarks) { landmark in
         NavigationLink {
-          LandmarkDetail(landmark: Landmark)
+          LandmarkDetail(landmark: landmark)
         } label: {
-          LandmarkRow(landmark: Landmark)
+          LandmarkRow(landmark: landmark)
         }
       }
       .navigationTitle("Landmarks")
@@ -25,10 +33,7 @@ struct LandmarkList: View {
 struct LandmarkList_Previews: PreviewProvider {
   static var previews: some View {
     
-    ForEach(["iPhone SE (2nd generation)", "iPhone XS Max"], id: \.self) { deviceName in
-        LandmarkList()
-            .previewDevice(PreviewDevice(rawValue: deviceName))
-            .previewDisplayName(deviceName)
-    }
+    LandmarkList()
+
   }
 }
