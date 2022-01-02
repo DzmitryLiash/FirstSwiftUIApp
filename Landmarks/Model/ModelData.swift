e68bbae29e0529d2d84b0aebbf48458bf72f91c5
Dzmitry Liashchou <liashchou.d@gmail.com>
2022-01-02 18:52:22 +0100

    section 5 adopt the model object in your views

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -9,9 +9,8 @@ import Foundation
 import Combine
 
 final class ModelData: ObservableObject {
-  
+ 
   @Published var landmarks: [Landmark] = load("landmarkData.json")
-
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
