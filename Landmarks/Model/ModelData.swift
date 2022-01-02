53b03355001efcea6893a5fee7c7b28457f50f89
Dzmitry Liashchou <liashchou.d@gmail.com>
2022-01-02 20:45:13 +0100

    section 2 create a category list

--- Landmarks/Model/ModelData.swift
+++ Landmarks/Model/ModelData.swift
@@ -13,6 +13,13 @@ final class ModelData: ObservableObject {
   @Published var landmarks: [Landmark] = load("landmarkData.json")
   var hikes: [Hike] = load("hikeData.json")
 
+  var categories: [String: [Landmark]] {
+      Dictionary(
+          grouping: landmarks,
+          by: { $0.category.rawValue }
+      )
+  }
+
 }
 
 func load<T: Decodable>(_ filename: String) -> T {
@@ -35,4 +42,5 @@ func load<T: Decodable>(_ filename: String) -> T {
   } catch {
     fatalError("Couldn't parse \(filename) as \(T.self):\n\(error)")
   }
+  
 }
